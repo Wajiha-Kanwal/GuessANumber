cab190af0cc77a22dab0484cf3de5b6874117a37
Wajiha Kanwal <waji.kanwal@yahoo.com>
2016-02-12 16:49:54 +0500

    Initial Commit

---
+++ CMDGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CMDGame
+//
+//  Created by Wajiha Kanwal on 12/02/2016.
+//  Copyright (c) 2016 Wajiha Kanwal. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
