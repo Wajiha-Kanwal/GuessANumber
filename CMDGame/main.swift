97d6612212ee0cc474600c27e8b524677bbdf8bf
Wajiha Kanwal <waji.kanwal@yahoo.com>
2016-02-12 17:14:53 +0500

    OSX command line app

--- CMDGame/main.swift
+++ CMDGame/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-println("Hello, World!")
+let answer = randomIntBetween(0, 100)
+var turns = 1
 
+while (true){
+    println("Guess #\(turns): Enter a number between 1 and 100.")
+    
+    let userInput = input()
+    
+    let inputAsInt = userInput.toInt()
+    // OR
+    // let inputAsInt = Int(userInput)
+    
+    if let guess = inputAsInt {
+        if(guess > answer) {
+            println("Lower!")
+        } else if(guess < answer) {
+            println("Higher!")
+        } else {
+            println("Correct! The answer was \(answer).")
+            break
+        }
+    }
+    else{
+        println("Invalid input! Please enter a number.")
+        continue
+    }
+    turns = turns + 1
+}
+println("It took you \(turns) turns")
